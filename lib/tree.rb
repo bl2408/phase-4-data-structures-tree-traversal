92cff3564597ad320184a787969e410e40809ac3
Brian Lambert <brianlambert2408@gmail.com>
2023-01-27 10:46:42 +1100

    Completed tasks

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,28 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id id
+    traverse id #, "breadth"
+  end
+
+  private
+
+  def traverse id, method="depth"
+    node_list = [ @root ]
+
+    while !node_list.empty?
+      if :method == "depth"
+        current_node = node_list.pop
+      else
+        current_node = node_list.shift
+      end
+      return current_node if current_node[:id] == id
+      
+      node_list = node_list + current_node[:children]
+    end
+
+    nil
+  end
+
 end
